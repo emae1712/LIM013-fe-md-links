7a6cde9e4488cbe118790fe33a56b3bd35c657f9
emae1712 <melissa.aechegaray@gmail.com>
2020-12-21 10:39:58 -0500

    test without fetch

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -5,14 +5,10 @@ const index = require('./index.js')
 const promiseMdLinks = (path, options = { validate: false }) => {
     return new Promise ((resolve, reject) => {
       if(!index.fileExist(path))reject(new Error(`No such file or directory '${path}' or is not a string`))
-      else if(index.getMdFiles(path).length == 0) reject(`Does not exist markdown files in this path '${path}'`)
-      else if(index.getLinks(path).length == 0) reject(`Does not exist links to evaluate in this path '${path}'`)
+      else if(index.getMdFiles(path).length === 0) reject(`Does not exist markdown files in this path '${path}'`)
+      else if(index.getLinks(path).length === 0) reject(`Does not exist links to evaluate in this path '${path}'`)
       else (options.validate === true) ? resolve(index.validate(path)) : resolve(index.getLinks(path))
     })
 };
 
 module.exports = promiseMdLinks;
-promiseMdLinks('test/Directory_test/folder_2', {validate: false})
-.catch((response)=>{
-  console.log(response);
-});
